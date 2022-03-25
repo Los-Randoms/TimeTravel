11dfa4ba0b69cbc11d16389636f3597085151bf0
Angel <ajimenez8@ucol.mx>
2022-03-25 10:37:41 -0600

    Se agregó datetime a comments

--- schemas/comments.sql
+++ schemas/comments.sql
@@ -1,4 +1,6 @@
 create table if not exists comments(
     id int not null auto_increment primary key,
     username int not null foreign key users(id),   --Ve los usuarios
+
+    date datetime not null,
 );
