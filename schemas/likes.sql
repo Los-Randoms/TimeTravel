841b49198047807dc71170834976cfef269d01b5
Angel <ajimenez8@ucol.mx>
2022-03-30 13:17:22 -0600

    Cambios en likes y tabla de publicaciónes

--- schemas/likes.sql
+++ schemas/likes.sql
@@ -1,7 +1,7 @@
 create table if not exists likes(
     id int not null auto_increment primary key,
     user int not null foreign key users(id),
-    date datetime not null
+    date datetime not null,
     public int not null foreign key publications(id)
 
 
