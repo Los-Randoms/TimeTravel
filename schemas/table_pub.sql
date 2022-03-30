a7e7d37fa855116b16ee9c980c91c4483fb473f0
Angel <ajimenez8@ucol.mx>
2022-03-30 13:30:53 -0600

    Añadí titulo a las publicaciones

--- schemas/table_pub.sql
+++ schemas/table_pub.sql
@@ -1,5 +1,6 @@
 create table if not exists publications(
     id int not null auto_increment primary key, 
+    tittle varchar(255) not null,
     image int not null foreign key files(id),
     date datetime not null,
     user int not null foreign key users(id),
