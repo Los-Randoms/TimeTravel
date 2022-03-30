d86413d015a086353604f2973537462e22b1d447
Angel <ajimenez8@ucol.mx>
2022-03-30 13:22:07 -0600

    agregué bodytext, referencias y fecha  en table pub

--- schemas/table_pub.sql
+++ schemas/table_pub.sql
@@ -2,5 +2,7 @@ create table if not exists publications(
     id int not null auto_increment primary key, 
     image int not null foreign key files(id),
     date datetime not null,
-    txt text not null
+    user int not null foreign key users(id),
+    txt text
+
 );
