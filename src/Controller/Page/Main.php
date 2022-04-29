f4fc90a70ebf9085d797b1b580b18e4196bce9d4
Mauricio <mcardenas@ucol.mx>
2022-04-29 01:21:34 -0500

    navbar phtml

--- src/Controller/Page/Main.php
+++ src/Controller/Page/Main.php
@@ -1,12 +1,10 @@
 <?php namespace Controller\Page;
 
 use Modules\Kernel\Page;
-use Modules\Kernel\Session;
 
 class Main extends Page {
 	function __construct() {
 		parent::__construct('index.phtml');
 		$this->setTitle('Time travel');
-		new Session();
 	}
 }
--- src/Modules/Kernel/Session.php
+++ src/Modules/Kernel/Session.php
@@ -1,12 +1,22 @@
 <?php namespace Modules\Kernel;
 
-class Session {
-	static private Session $current;
-	private string $ssid;
-	private User $user;
+abstract class Session {
+	public static function isLogged(): bool {
+		$cookie_name = session_name();
+		if(!isset($_COOKIE[$cookie_name]))
+			return false;
+		session_start();
+		if(!isset($_SESSION['uid'])) {
+			session_destroy();
+			return false;
+		}
+		return true;
+	}
 
-	public function __construct() {
-		echo session_name();
-		die;
+	public static function currentUser(): ?User {
+		if(!static::isLogged())
+			return null;
+		$user = User::load($_SESSION['uid']);
+		return $user;
 	}
 }
