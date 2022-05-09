fb38da548158aa18bff413dc297b4ee6fa5c26ef
NuboDub0 <98866174+NeftaliBa@users.noreply.github.com>
2022-05-09 13:02:11 -0500

    modificiación al archivo css, punto de guardado por si acaso

--- src/Controller/Form/Changep.php
+++ src/Controller/Form/Changep.php
@@ -10,30 +10,30 @@ class Changep extends Form {
     function __construct()
     {
         parent::__construct('changep.phtml');
-        $this->Style('css/changep.css');
-        $this->Title('Cambiar contraseña');
+        $this->style('css/changep.css');
+        $this->title('Cambiar contraseña');
     }
 
     public function verify(): bool
     {
-        if(!isset($_POST['correo']) || empty($_POST['correo']))
+        if(!isset($_POST['email']) || empty($_POST['email']))
             return false;   
         return true;
     }
 
     public function _submit()
     {
-        print_r($_POST['correo']);
         /** @var \Modules\Mysql\Driver */
         $driver=Storage::driver();
         $select=$driver->read(AccountUser::TABLE);
-        $select->condition('email', $correo);
+        $select->condition('email', $_POST['email'], 's', '=');
+        $select->select('password');
         $user=$select->execute();
+
 
        
        
        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz$#';
-       $correo = $_POST["correo"];
        $newp=substr(str_shuffle($permitted_chars), 0, 8); //genera carcteres aleatorio
        //fetch_object es para que los resultados de la busqueda se guarden como un objeto, y para poder buscar un objeto 
        //se puede poner print_r($res->"Lo que sea que estes buscando");
@@ -46,6 +46,6 @@ class Changep extends Form {
        //mail($correo, 
        //'Cambio de contraseña',  
        //"Hemos recibido su solicitud de cambio de contraseña y su nueva contraseña es: $newp");
-       
-    }
-}
+        
+    } 
+} 
