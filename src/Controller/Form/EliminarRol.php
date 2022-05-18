d199a9e1f1b78aa2ec46869e0eb5ee6a970828f9
esteban-od1n <lalcaraz11@ucol.mx>
2022-05-18 11:34:03 -0500

    Eliminar Rol, no listo

--- src/Controller/Form/EliminarRol.php
+++ src/Controller/Form/EliminarRol.php
@@ -10,7 +10,7 @@ class EliminarRol extends Form {
         $this->style('css\eliminarroles.css');
         $db = Storage::driver();
         /** @var \Modules\Mysql\Query\Select */
-        $consulta = $db->read('roles');                  // se lee la tabla de roles
+        $consulta = $db->read('roles');                 // se lee la tabla de roles
         $consulta->orderBy('id');                       // ordenadorlos por Id
         $consulta->execute();                           //se ejecuta
         $this->roles = $consulta->results();            //la tabla de guarda en una variable
@@ -22,21 +22,14 @@ class EliminarRol extends Form {
         $_POST['id'] = trim($_POST['id']);              //no dejar espacios
         if(empty($_POST['id']))                         //no este vacio
             $this->error("Selecciona un rol");          //error
-
-        if(!isset($_POST['nombre']))
-            $this->error('Ingresa un nombre');
-        $_POST['nombre'] = trim($_POST['nombre']);
-        if(empty($_POST['nombre']))
-            $this->error("Ingresa un nombre");
     }
 
     function _submit(): ?string {                       //parte de realización
         $db = Storage::driver();
-        /** @var \Modules\Mysql\Query\Update */
-        $consulta = $db->update('roles');               //seleciona la tabla de roles
+        /** @var \Modules\Mysql\Query\Delete */
+        $consulta = $db->delete('roles');               //seleciona la tabla de roles
         $consulta->condition('id', $_POST['id']);       // se actualiza el nombre con tal id
-        $consulta->set('name', $_POST['nombre']);       // se establece
         $consulta->execute();                           //termina
-        return 'Se modifico el nombre correctamente';   //manda el mensaje
+        return 'Se elimino el rol correctamente';   //manda el mensaje
     }
 }
